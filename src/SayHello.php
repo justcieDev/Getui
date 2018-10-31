51262b7ffa901d1a3a042b46c8c93a0c122405df
实地张书诚 <shucheng.zhang@serviceindeed.com>
2018-10-31 15:22:07 +0800

    First commit

---
+++ src/SayHello.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: justice
+ * Date: 2018/10/31
+ * Time: 2:55 PM
+ */
+namespace Getui;
+
+class SayHello {
+    public static function world()
+    {
+        return 'Hello World!';
+    }
+}
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use Getui\SayHello;
+echo SayHello::world();
+
+//echo SayHello::world();
